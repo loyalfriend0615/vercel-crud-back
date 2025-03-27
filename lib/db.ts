4931cde74e44309eb7b8c3e4c4b370314f5a02ab
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-03-26 21:02:41 -0700

    database_url added

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg"
 
 // Create a new pool instance with your connection string
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "postgresql://vercel-crud-app_owner:npg_sOw3CR5xHeVf@ep-gentle-band-a2p3srx9.eu-central-1.aws.neon.tech/vercel-crud-app?sslmode=require",
   ssl: {
     rejectUnauthorized: false, // Required for some PostgreSQL providers like Neon
   },
